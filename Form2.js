cf35d392bd898c7139ff2b6bf4d0a11d69729d36
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-05-02 10:55:26 +0530

    Basic validation for Login form

--- Form2.js
+++ Form2.js
@@ -3,24 +3,60 @@ function onSubmit(e) {
     e.preventDefault();
 }
 
+var username = document.getElementById('username')
+var password = document.getElementById('password')
+
+var userSpan = document.getElementById('userSpan')
+var passSpan = document.getElementById('passSpan')
 
 
 function validation() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+    var usernameValid = username.value;
+    var passwordValid = password.value;
 
     // alert("hello " + username + " " + password)
 
-    if(username == 'rohit'){
-        if(password == 'R1234'){
-            alert(`User ${username}!!! You are successfully Login`)
+    if(usernameValid == 'rohit'){
+        if(passwordValid == 'R1234'){
+            alert(`User ${usernameValid} !!! You are successfully Login`)
         }else{
-            alert(`User ${username}!!!  Login Failed`)
+            alert(`User ${usernameValid} !!!  Login Failed    Your Password are Incorrect`)
         }
     }else{
         alert("User Not Found")
     }
 }
+
+function validUser(){
+    console.log(document.getElementById("username").value)
+    var u_length = username.value.length;
+    console.log(u_length)
+
+    if(u_length <= 3){
+        userSpan.innerText = 'User Not valid'
+    }
+    return true;
+
+    // const bg = () => {
+    //     document.getElementsByClassName('inUserDiv').style = "red"
+    // }
+
+    // bg();
+}
+
+
+function validPass(){
+    var p_length = document.getElementById('password').value.length;
+
+    console.log(p_length)
+
+    if(p_length <= 3){
+        passSpan.innerText = 'Password Not valid'
+    }
+    return true;
+}
+
+
 
 
 
