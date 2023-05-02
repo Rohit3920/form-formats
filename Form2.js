17c259fd57d2c152af44b6c6fc26f94a2f0632e2
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-05-02 11:18:04 +0530

    same basic form validation < border color change > and code formating extra spacing remove

--- Form2.js
+++ Form2.js
@@ -1,63 +1,50 @@
-
 function onSubmit(e) {
     e.preventDefault();
 }
 
 var username = document.getElementById('username')
 var password = document.getElementById('password')
-
 var userSpan = document.getElementById('userSpan')
 var passSpan = document.getElementById('passSpan')
-
 
 function validation() {
     var usernameValid = username.value;
     var passwordValid = password.value;
 
-    // alert("hello " + username + " " + password)
-
-    if(usernameValid == 'rohit'){
-        if(passwordValid == 'R1234'){
+    if (usernameValid == 'rohit') {
+        if (passwordValid == 'R1234') {
             alert(`User ${usernameValid} !!! You are successfully Login`)
-        }else{
+        } else {
             alert(`User ${usernameValid} !!!  Login Failed    Your Password are Incorrect`)
         }
-    }else{
+    } else {
         alert("User Not Found")
     }
 }
 
-function validUser(){
-    console.log(document.getElementById("username").value)
+function validUser() {
     var u_length = username.value.length;
-    console.log(u_length)
 
-    if(u_length <= 3){
+    if (u_length <= 3) {
         userSpan.innerText = 'User Not valid'
+        document.getElementById('inUserDiv').style.borderColor = "red";
+    } else {
+        userSpan.innerText = ""
+        document.getElementById('inUserDiv').style.borderColor = "black";
     }
     return true;
-
-    // const bg = () => {
-    //     document.getElementsByClassName('inUserDiv').style = "red"
-    // }
-
-    // bg();
 }
 
-
-function validPass(){
+function validPass() {
     var p_length = document.getElementById('password').value.length;
 
-    console.log(p_length)
-
-    if(p_length <= 3){
+    if (p_length <= 3) {
         passSpan.innerText = 'Password Not valid'
+        document.getElementById('inPassDiv').style.borderColor = "red";
+    } else {
+        passSpan.innerText = ''
+        document.getElementById('inPassDiv').style.borderColor = "black";
     }
+
     return true;
 }
-
-
-
-
-
-
